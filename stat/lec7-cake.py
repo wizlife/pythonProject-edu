2431c36dabfc10a348ba599f04570010f637ca35
jspark <bewise@gmail.com>
2024-04-10 16:53:19 +0900

    수업과제 등록

--- stat/lec7-cake.py
+++ stat/lec7-cake.py
@@ -6,7 +6,7 @@ price = 25000
 cost = 5000
 
 # 최대로 준비할 수 있는 케이크 수
-max_cakes = 50
+max_cakes = 30
 # 각각의 케이크 수에 대한 수익 배열
 # 0개 수익, 1개 수익 ...
 profit = []
@@ -17,8 +17,17 @@ probabilities = [math.exp(-17) * 17 ** k / math.factorial(k) for k in range(max_
 
 for num_cakes in range(1, max_cakes+1):
     # 예상 수익
-    expected_profit = sum(probabilities[:num_cakes+1] * np.arange(num_cakes+1) * price) - (num_cakes * cost)
-    # 첫번째가 1개 만들었을때의 수익
+    expected_profit = 0
+    for sell_cake in range(0, num_cakes):
+        expected_prob = 1.0 - sum(probabilities[:sell_cake])
+        expected_profit += expected_prob * sell_cake * price - (num_cakes * cost)
+
+    # 케익이 팔릴 확률 = 케이크를 1개 팔릴 확률 + 케이크 2개 팔릴 확률 + ... + 준비한 케이크까지 팔릴확률
+    #
+    # expected_profit = sum(expected_probability * )
+
+
+    # 첫번째 엘레먼트가 케이크를 1개 만들었을때의 최대 수익
     profit.append(expected_profit)
 
 # 최대 예상 수익과 그 때의 케이크 수 출력
